ada8e17b3ab6a2cc3ad4f471b77c460dba5a66a3
just9n <nguyentrongtin2331@gmail.com>
2020-04-15 21:26:47 +0700

    fix variable not  found

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ initDb(function (err) {
 
   app.get("/crawl-matches", (req, res, next) => {
     const db = getDb().db();
+    const hltv = getHLTV();
 
     hltv.getMatchesStats({startDate: req.query.startDate, endDate: req.query.endDate, rankingFilter: req.query.rankingFilter}).then((matches) => {
       matches.forEach(function(item){

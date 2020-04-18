b65afb389ef3f798561a0d16d28656b7716dd610
just9n <nguyentrongtin2331@gmail.com>
2020-04-19 01:08:41 +0700

    refactor cron to accept promise

--- cron.js
+++ cron.js
@@ -18,97 +18,58 @@ initDb(function (err) {
   createHLTV()
 });
 
-var _updateUpcomingMatch = function() {
+var _updateUpcomingMatch = async function() {
   const db = getDb().db();
   const hltv = getHLTV();
 
-  db.collection('upcoming_and_live_matches').findOne({details: null}, function(err, target){
-    if (!target) return 
-    console.log("trying to update upcoming match %s", target._id)
-    hltv.getMatch({id: target.id}).then(res => {
-      db.collection('upcoming_and_live_matches').updateOne({_id: target._id}, {$set: {"details": res}}, errorHandler("update upcoming match with id = " + target._id))
-    }).catch(eHandler)
-  })
+  var target = await db.collection('upcoming_and_live_matches').findOne({details: null}).catch(errorHandler("find one upcoming match with stats = null"))
+  var res = await hltv.getMatch({id: target.id}).catch(eHandler)
+  return db.collection('upcoming_and_live_matches').updateOne({_id: target._id}, {$set: {"details": res}}).catch(errorHandler("update upcoming match with id = " + target._id))
 }
 
-var _crawlUpcomingMatches = function() {
+var _crawlUpcomingMatches = async function() {
   const db = getDb().db();
   const hltv = getHLTV();
 
-  hltv.getMatches().then((matches) => {
-    matches.forEach(function(match){
-      db.collection('upcoming_and_live_match').findOne({"match.id": match.id}, function(err, target){
-        if (target == null)
-          db.collection('upcoming_and_live_match').insertOne({"match": match}, errorHandler("insert 1 upcoming match"))
-        else
-          db.collection('upcoming_and_live_match').updateOne({_id: target._id}, {$set: {"match": match}}, errorHandler("update 1 upcoming match"))
-
-      })
-    })
-  }).catch(eHandler)
+  var matches = await hltv.getMatches().catch(eHandler)
+  for (const match of matches) {
+      var target = db.collection('upcoming_and_live_match').findOne({"match.id": match.id}).catch(errorHandler("finding 1 upcoming matcch with id = " + match.id))
+      if (target == null)
+        db.collection('upcoming_and_live_match').insertOne({"match": match}).catch(errorHandler("insert 1 upcoming match"))
+      else
+        db.collection('upcoming_and_live_match').updateOne({_id: target._id}, {$set: {"match": match}}).catch(errorHandler("update 1 upcoming match"))
+  }
 }
 
-var _updateTeam = function() {
+var _updateTeam = async function() {
   const db = getDb().db();
   const hltv = getHLTV();
 
-  db.collection('teams').findOne({ stats: null }, function(err, team){
-    if (err) {
-      console.error(err)
-      return
-    }
-    if (team == null) return;
-    hltv.getTeamStats({id: team.team.id}).then((team_stat) => {
-      console.log("update team stats ", team._id)
-      if (team_stat === null) {
-        return
-      }
-      db.collection('teams').updateOne({_id: team._id}, {$set: {"stats": team_stat}}, errorHandler("updating team with id = " + team._id));
-    }).catch(eHandler)
-  });
+  var team = await db.collection('teams').findOne({ stats: null }).catch(errorHandler("finding team with stats = null"))
+  var team_stat = await hltv.getTeamStats({id: team.team.id})
+  return db.collection('teams').updateOne({_id: team._id}, {$set: {"stats": team_stat}}).catch(errorHandler("updating team with id = " + team._id));
 }
 
-var _updateSeries = function() {
+var _updateSeries = async function() {
   const db = getDb().db();
   const hltv = getHLTV();
 
-  db.collection('match_maps').findOne({ update_series: null, stats: {$exists: true} }, function(err, result){
-    if (err) {
-      console.error(err)
-      return
-    }
-    if (result == null) return;
-    db.collection('matches').findOne({"match.id": result.stats.matchPageID}, function(err, match){
-      if (match == null) {
-        hltv.getMatch({id: result.stats.matchPageID}).then((match) => {
-          db.collection('matches').insertOne({match: match}, errorHandler("Inserting 1 serie with id = " + match.id))
-        }).catch(eHandler)
-      }
-      db.collection('match_maps').updateOne({_id: result._id}, {$set: {update_series: true}}, errorHandler("Updating 1 map with updated_serie, id = " + result._id))
-    })
-  });
+  var match_map = await db.collection('match_maps').findOne({ update_series: null, stats: {$exists: true} }).catch(errorHandler("finding 1 maps don't have series"))
+  var match = await db.collection('matches').findOne({"match.id": result.stats.matchPageID}).catch(errorHandler("find match with id = " + result.stats.matchPageID))
+  if (match == null) {
+    var match = await hltv.getMatch({id: result.stats.matchPageID}).catch(eHandler)
+    await db.collection('matches').insertOne({match: match}).catch(errorHandler("Inserting 1 serie with id = " + match.id))
+  }
+  return db.collection('match_maps').updateOne({_id: result._id}, {$set: {update_series: true}}).catch(errorHandler("Updating 1 map with updated_serie, id = " + result._id))
 }
 
-var _updateMapStats = function() {
+var _updateMapStats = async function() {
   const db = getDb().db();
   const hltv = getHLTV();
 
-  db.collection('match_maps').findOne({ stats: null }, function(err, match_map){
-    if (err) {
-      console.error(err)
-      return
-    }
-    if (match_map == null) return;
-    hltv.getMatchMapStats({id: match_map.id}).then((map_stat) => {
-      db.collection('match_maps').updateOne({_id: match_map._id}, {$set: {"stats": map_stat}}, function(err, res) {
-        if (err) {
-          console.error("error when crawl map stats", match_map.id, err)
-          return
-        }
-        console.log("Number of match updated: 1");
-      });
-    }).catch(eHandler)
-  });
+  var match_map = await db.collection('match_maps').findOne({ stats: null }).catch(errorHandler(""))
+  var map_stat = await hltv.getMatchMapStats({id: match_map.id}).catch(eHandler)
+  return db.collection('match_maps').updateOne({_id: match_map._id}, {$set: {"stats": map_stat}}).catch(errorHandler("updating 1 match_maps, set stats with map_id = " + match_map.id))
 }
 
 function dis(dateObj){
@@ -127,89 +88,91 @@ function yesterday(unix) {
   return dis(o)
 }
 
-var _updateSeriesPlayerStats = function() {
-}
-
-var _updateSeriesTeamExtraStats = function() {
+var _updateSeriesPlayerStats = async function() {
   const db = getDb().db();
   const hltv = getHLTV();
 
-  db.collection('matches').findOne({ teams_extraStats: null }, function(err, match){
-    if (match == null) return;
-    var f = threeMonthsAgo(match.match.date)
-    var t = yesterday(match.match.date)
-
-    hltv.getTeamExtraStats({startDate: f, endDate: t, rankingFilter: TOP_50}).then((teams) => {
-      db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_extraStats.team1": teams[match.match.team1.id]}}, errorHandler("updating team1.ExtraStats in match with id = " + match._id));
-      db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_extraStats.team2": teams[match.match.team2.id]}}, errorHandler("updating team2.ExtraStats in match with id = " + match._id));
-    }).catch(eHandler)
-  });
-}
-
-var _updateSeriesTeamStats = function() {
-  const db = getDb().db();
-  const hltv = getHLTV();
-
-  db.collection('matches').findOne({ teams_stats: null }, function(err, match){
-    if (match == null) return;
-    var f = threeMonthsAgo(match.match.date)
-    var t = yesterday(match.match.date)
-
-    hltv.getTeamStats({id: match.match.team1.id, startDate: f, endDate: t, rankingFilter: TOP_50}).then((team_stat) => {
-      db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_stats.team1": team_stat}}, errorHandler("updating team1.stats in match with id = " + match._id));
-    }).then(() => {
-      hltv.getTeamStats({id: match.match.team2.id, startDate: f, endDate: t, rankingFilter: TOP_50}).then((team_stat) => {
-        db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_stats.team2": team_stat}}, errorHandler("updating team2.stats in match with id = " + match._id));
-      }).catch(eHandler)
-    }).catch(eHandler)
-  });
-}
-
-var _updateSeriesStats = function() {
-  const db = getDb().db();
-  const hltv = getHLTV();
-
-  db.collection('matches').findOne({ stats: null }, function(err, match){
-    if (err) {
-      console.error(err)
-      return
+  var match = await db.collection('matches').findOne({ players_stats: null }).catch(errorHandler("find matches with player_stats is null"))
+  var f = threeMonthsAgo(match.match.date)
+  var t = yesterday(match.match.date)
+  var  playersStats = []
+  var getPlayerStat = function* () {
+    for(var i = 0;i< 10;i++) {
+      if (i < 5) {
+        yield hltv.getPlayerStats({id: match.match.players.team1[i].id, startDate: f, endDate: t, rankingFilter: TOP_50}).catch(eHandler)
+      } else {
+        yield hltv.getPlayerStats({id: match.match.players.team2[i-5].id, startDate: f, endDate: t, rankingFilter: TOP_50}).catch(eHandler)
+      }
     }
-    if (match == null) return;
-    console.log(match.match.statsId)
-    if (match.match.format != "Best of 1"){
-      hltv.getMatchStats({id: match.match.statsId}).then((match_stat) => {
-        db.collection('matches').updateOne({_id: match._id}, {$set: {"stats": match_stat}}, errorHandler("Update match stat with id = " + match._id));
-      }).catch(eHandler)
-    } else {
-      db.collection('matches').updateOne({_id: match._id}, {$set: {"stats": true}}, errorHandler("Update match stat with id = " + match._id));
-    }
-  });
+  }
+    
+  for await (const playerStat of getPlayerStat()) {
+    playersStats.push(playerStat)
+    console.log("got 1")
+  }
+
+  return db.collection('matches').updateOne({_id: match._id}, {$set: {"players_stats": playersStats}}).catch(errorHandler("updating players stats in match with id = " + match._id));
 }
 
+var _updateSeriesTeamExtraStats = async function() {
+  const db = getDb().db();
+  const hltv = getHLTV();
 
-var _crawlNewMaps = function() {
+  var match = await db.collection('matches').findOne({ teams_extraStats: null }).catch(errorHandler("find 1 match with team_extraStats = null"))
+  var f = threeMonthsAgo(match.match.date)
+  var t = yesterday(match.match.date)
+
+  var teams = await hltv.getTeamExtraStats({startDate: f, endDate: t, rankingFilter: TOP_50}).catch(eHandler)
+  db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_extraStats.team1": teams[match.match.team1.id]}}).catch(errorHandler("updating team1.ExtraStats in match with id = " + match._id));
+  db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_extraStats.team2": teams[match.match.team2.id]}}).catch(errorHandler("updating team2.ExtraStats in match with id = " + match._id));
+}
+
+var _updateSeriesTeamStats = async function() {
+  const db = getDb().db();
+  const hltv = getHLTV();
+
+  var match = await db.collection('matches').findOne({ teams_stats: null }).catch(errorHandler("finding matches with team_stats = null"))
+  var f = threeMonthsAgo(match.match.date)
+  var t = yesterday(match.match.date)
+
+  var team_stat = await hltv.getTeamStats({id: match.match.team1.id, startDate: f, endDate: t, rankingFilter: TOP_50}).catch(eHandler)
+  await db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_stats.team1": team_stat}}).catch(errorHandler("updating team_stats.team1 in match with id = "+match._id))
+  var team_1stat = await hltv.getTeamStats({id: match.match.team2.id, startDate: f, endDate: t, rankingFilter: TOP_50}).catch(eHandler)
+  await db.collection('matches').updateOne({_id: match._id}, {$set: {"teams_stats.team2": team_1stat}}).catch(errorHandler("updating team_stats.team2 in match with id = "+match._id))
+}
+
+var _updateSeriesStats = async function() {
+  const db = getDb().db();
+  const hltv = getHLTV();
+
+  var match = await db.collection('matches').findOne({ stats: null }).catch(errorHandler("find 1 match with stats = null"))
+  if (match.match.format != "Best of 1"){
+    var match_stat = await hltv.getMatchStats({id: match.match.statsId}).catch(eHandler)
+    return db.collection('matches').updateOne({_id: match._id}, {$set: {"stats": match_stat}}).catch(errorHandler("Update match stat with id = " + match._id));
+  } else {
+    return db.collection('matches').updateOne({_id: match._id}, {$set: {"stats": true}}).catch(errorHandler("Update match stat with id = " + match._id));
+  }
+}
+
+var _crawlNewMaps = async function() {
   const db = getDb().db();
   const hltv = getHLTV();
 
   var t = new Date()
   var f = new Date(new Date().setDate(t.getDate()-1))
 
-  hltv.getMatchesStats({startDate: dis(f), endDate: dis(t), rankingFilter: TOP_50}).then((matches) => {
-    matches.forEach(function(item){
-      db.collection('match_maps').findOne({id: item.id}, function(err, result){
-        if (result !== null) {
-          console.warn("trying to insert exited match", item.id)
-          return
-        }
-        console.log("insert new map")
-        db.collection('match_maps').insertOne(item, errorHandler("inserting new maps with id = " + item.id));
-      });
-    })
-  }).catch(eHandler)
+  var matches = await hltv.getMatchesStats({startDate: dis(f), endDate: dis(t), rankingFilter: TOP_50})
+  for (const match in matches) {
+    var match_maps = db.collection('match_maps').findOne({id: match.id}).catch(errorHandler("finding 1 maps with id = " + match.id))
+    if (match_maps !== null) continue
+    db.collection('match_maps').insertOne(match, errorHandler("inserting new maps with id = " + match.id));
+  }
 }
 
-var runJob = function(){
+var jobManager = function(){
   var jobIndex = 0
+  console.log("init busy")
+  var busy = false
   const jobList = [
     {
       name: 'crawlUpcomingMatches',
@@ -239,21 +202,32 @@ var runJob = function(){
       name: 'updateSeriesTeamStats',
       handler: _updateSeriesTeamStats,
     },
+    {
+      name: 'updateSeriesPlayerStats',
+      handler: _updateSeriesPlayerStats,
+    },
   ]
 
-  return function(){
-    console.log("starting cron job %s", jobList[jobIndex].name)
-    jobList[jobIndex].handler()
+  return {
+    execute: async function(){
+      if (busy) return
+      console.log("starting cron job %s", jobList[jobIndex].name)
+      busy = true
+      await jobList[jobIndex].handler().catch(jobManager.errorHandler)
+      busy = false;
 
-    if (jobIndex == jobList.length-1)
-      jobIndex = 0;
-    else
-      jobIndex++;
+      if (jobIndex == jobList.length-1)
+        jobIndex = 0;
+      else
+        jobIndex++;
+    },
+    reset: () => busy = false,
+    errorHandler: (err) => console.error(err)
   }
 }()
 
 function startCronScheduler(){
-  var job = new CronJob(process.env.CRON_FORMAT, runJob, null, false);
+  var job = new CronJob(process.env.CRON_FORMAT, jobManager.execute, null, false);
   job.start()
 }
 
